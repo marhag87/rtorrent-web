35b2d8f60041aff68ab4a1cc6121a311765fe69e
Martin Hagstrom <marhag87@gmail.com>
2016-11-21 21:29:09 +0100

    Add simple filtering from sidebar

--- rtorrent-web.py
+++ rtorrent-web.py
@@ -10,27 +10,32 @@ app = Flask(__name__)
 _CLIENTS = load_config('config.yaml').get('clients')
 
 
-def fetch_torrents(client, sort=True):
+def fetch_torrents(client, active, sort=True):
     rtorrent = Rtorrent(client.get('url'))
     torrents = rtorrent.all_torrents()
     if sort:
         torrents.sort(key=lambda x: x.name)
-    return {
+    my_dict = {
         'torrents': torrents,
         'title': client.get('title'),
     }
+    if active == client.get('title'):
+        my_dict['active'] = True
+    return my_dict
 
 
 @app.route('/')
-def dashboard():
+@app.route('/<active>')
+def dashboard(active=False):
     clients = []
     for client in _CLIENTS:
         clients.append(
-            fetch_torrents(client),
+            fetch_torrents(client, active),
         )
     return render_template(
         'index.html',
         clients=clients,
+        active=active,
     )
 
 if __name__ == '__main__':
