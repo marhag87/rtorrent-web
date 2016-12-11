b0bcfbc97a92592434dc834efdb92787f06f2b4f
Martin Hagstrom <marhag87@gmail.com>
2016-12-11 14:55:25 +0100

    Add settings page for adding and removing clients
    Fixes #12

--- rtorrent_web.py
+++ rtorrent_web.py
@@ -3,8 +3,14 @@ from flask import (
     Flask,
     render_template,
     request,
+    redirect,
+    url_for,
 )
-from pyyamlconfig import load_config
+from pyyamlconfig import (
+    load_config,
+    write_config,
+    PyYAMLConfigError,
+)
 from pyrtorrent import Rtorrent
 from jinja2.filters import do_filesizeformat
 
@@ -45,6 +51,8 @@ def fetch_torrents(client, active, search, sort=True):
 @app.route('/')
 @app.route('/torrents/')
 def route():
+    if not app.config.get('clients'):
+        return redirect(url_for('settings'))
     active = False if request.args.get('active') == 'active' else request.args.get('active')
     search = None if request.args.get('search') == 'search' else request.args.get('search')
     clients = []
@@ -97,7 +105,104 @@ def mark_all_seen():
 
     return "Marked all as seen"
 
+
+@app.route('/settings')
+def settings():
+    clients = []
+    for client in app.config.get('clients'):
+        clients.append(client)
+    return render_template(
+        'settings.html',
+        clients=clients,
+    )
+
+
+def has_duplicates(resource, key):
+    for outer in resource:
+        num_found = 0
+        for inner in resource:
+            if inner.get(key) == outer.get(key):
+                num_found += 1
+        if num_found > 1:
+            return True
+    return False
+
+
+def validate_client(url):
+    try:
+        Rtorrent(url).all_torrents()
+        return True
+    except:
+        return False
+
+
+@app.route('/settings/add_client/<title>/<path:url>')
+def add_client(title, url):
+    duplicate_title = has_duplicates(
+        [
+            *app.config['clients'],
+            {'url': url, 'title': title},
+        ],
+        'title',
+    )
+    duplicate_url = has_duplicates(
+        [
+            *app.config['clients'],
+            {'url': url, 'title': title},
+        ],
+        'url',
+    )
+    message = 'Could not add client, duplicate {} found'
+    if duplicate_title or duplicate_url:
+        dups = []
+        if duplicate_title:
+            dups.append('title')
+        if duplicate_url:
+            dups.append('url')
+        return message.format(' and '.join(dups)), 400
+    else:
+        if validate_client(url):
+            app.config['clients'].append({'url': url, 'title': title})
+            _CONFIG['clients'] = app.config['clients']
+            write_config('config.yaml', _CONFIG)
+            return "Added client"
+        else:
+            return "Could not add client, url invalid", 400
+
+
+@app.route('/settings/remove_client/<title>')
+def remove_client(title):
+    app.config['clients'] = [x for x in app.config['clients'] if x.get('title') != title]
+    _CONFIG['clients'] = app.config['clients']
+    write_config('config.yaml', _CONFIG)
+    return "Removed client"
+
+
+@app.route('/configure')
+def configure():
+    clients = []
+    for client in app.config.get('clients'):
+        clients.append(client)
+    return render_template(
+        'configure.html',
+        clients=clients,
+    )
+
+
+@app.route('/sidebar')
+def sidebar():
+    clients = []
+    for client in app.config.get('clients'):
+        clients.append(client)
+    return render_template(
+        'sidebar.html',
+        clients=clients,
+    )
+
 if __name__ == '__main__':
-    _CONFIG = load_config('config.yaml')
-    app.config['clients'] = _CONFIG.get('clients')
-    app.run(debug=_CONFIG.get('debug'))
+    try:
+        _CONFIG = load_config('config.yaml')
+    except PyYAMLConfigError:
+        _CONFIG = {}
+    app.config['clients'] = _CONFIG.get('clients', [])
+    app.run(debug=_CONFIG.get('debug', False))
